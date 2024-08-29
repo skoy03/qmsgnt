015b688cadce990997bb803d00841d66d7187eda
skoy03 <69272126+skoy03@users.noreply.github.com>
2024-08-29 17:23:40 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-if [ ! -f "/usr/src/app/QmsgNtClient-NapCatQQ/package.json" ]; then
-    unzip /tmp/QmsgNtClient-NapCatQQ.zip -d /usr/src/app/QmsgNtClient-NapCatQQ
+if [ ! -f "/usr/src/app/QmsgNtClient-NapCatQQ/config/napcat.json" ]; then
+    mv /tmp/QmsgNtClient-NapCatQQ/config/* /usr/src/app/QmsgNtClient-NapCatQQ/config/ && rm -rf /tmp/QmsgNtClient-NapCatQQ
 fi
-
 chmod -R 777 /usr/src/app/QmsgNtClient-NapCatQQ/
-
+Xvfb :1 -screen 0 1080x760x16 +extension GLX +render &
 export FFMPEG_PATH=/usr/bin/ffmpeg
-
-cd ./QmsgNtClient-NapCatQQ
-./napcat.sh -q $ACCOUNT
+export DISPLAY=:1
+qq --no-sandbox -q $ACCOUNT
